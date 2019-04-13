f6151d93e87da59ad319c15cdb3a2a7c054746f6
wuhaoqiu <whq672437089@outlook.com>
2019-04-12 17:58:48 -0700

    use absolute path
    use absolute path in mlmodels/predict and preprocess to store data, read data, and invoke model.h5

--- mysite/mlmodels/shape_predict/shape_predict_model.py
+++ mysite/mlmodels/shape_predict/shape_predict_model.py
@@ -1,5 +1,7 @@
 #author:Haoqiu Wu Time 19.4.12
-
+from scipy.io import loadmat
+import numpy as np
+import tensorflow as tf
 
 def store_file(input_data):
     # overwite the file if it exists
@@ -8,21 +10,32 @@ def store_file(input_data):
             destination.write(chunk)
 
 def predict():
-    from scipy.io import loadmat
-    import numpy as np
-    import tensorflow as tf
     try:
-        x = loadmat('mysite/media/shape_predict/uploaded/data.mat')
+        x = loadmat('C:/Users/whq672437089/Envs/engr597-unstable/mysite/media/shape_predict/uploaded/data.mat')
         input_data = np.zeros((1, 100, 3))
         x_values = list(x.values())
         x_array = np.array(x_values[-1])
         if x_array.shape != (3, 100):
-            return 'data dimension is not correct, dimension should be (1,100,3)'
+            return 'data dimension is not correct, dimension should be (3,100)'
         else:
             input_data[None, :, :] = x_array.T
-            model2 = tf.keras.models.load_model('mysite/shape_predict/models/movement_shape_predict.h5')
+            model2 = tf.keras.models.load_model('C:/Users/whq672437089/Envs/engr597-unstable/mysite/media/shape_predict/models/movement_shape_predict.h5')
             d = {0: 'bad curve', 1: 'medium curve', 2: 'good curve'}
             predict_result=d[np.argmax(model2.predict(input_data))]
         return predict_result
     except Exception:
         return 'errors happen when reading uploaded data'
+
+if __name__=='__main__':
+    x = loadmat('C:/Users/whq672437089/Envs/engr597-unstable/mysite/media/shape_predict/uploaded/data.mat')
+    input_data = np.zeros((1, 100, 3))
+    x_values = list(x.values())
+    x_array = np.array(x_values[-1])
+    if x_array.shape != (3, 100):
+        print('yes')
+    else:
+        input_data[None, :, :] = x_array.T
+        model2 = tf.keras.models.load_model(
+            'C:/Users/whq672437089/Envs/engr597-unstable/mysite/media/shape_predict/models/movement_shape_predict.h5')
+        d = {0: 'bad curve', 1: 'medium curve', 2: 'good curve'}
+        predict_result = d[np.argmax(model2.predict(input_data))]
